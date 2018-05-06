5d8256b7dcdb4b8e9af8b3fe1098acdb0a01ea03
Anshika Singh <anshika135singh@gmail.com>
2018-05-06 17:27:49 -0500

    Validations added

--- digital-identity/routes/api.js
+++ digital-identity/routes/api.js
@@ -3,6 +3,9 @@ var router = express.Router();
 var multer = require('multer');
 var dateformat = require('dateformat');
 var cryptoJS = require('crypto-js');
+const { check,validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+
 const IdentityRegistry = require('../lib/identityregistry');
 var NodeRSA = require('node-rsa');
 var fs = require('fs');
@@ -29,8 +32,11 @@ router.get('/', function(req, res, next) {
    res.send("API")
 });
 
-router.route('/official')
-    .post(function(req,res,next){
+router.route('/official').post([
+        check('id').exists().isInt(),
+        check('name').exists().matches(/(a-zA-Z)*\s/),
+        check('designation').exists().matches(/(a-zA-Z)*\s/),
+    ],function(req,res,next){
         var idReg = new IdentityRegistry();
         idReg.init().then(function(){
             idReg.addOfficial(req.body).then(function success(){
@@ -41,23 +47,40 @@ router.route('/official')
         });
     });
 
-router.post('/passport',mulUpload,function(req,res){
-    var files = req.files;
-    var idPhotoStr = req.files['idPhoto'][0].buffer.toString('base64');
-    var signPhotoStr = req.files['signPhoto'][0].buffer.toString('base64');
-    var passportData = {
-        'formData':req.body,
-        'idPhotoStr': idPhotoStr,
-        'signPhotoStr': signPhotoStr
-    };
-    var idReg = new IdentityRegistry();
-    idReg.init().then(function(){
-        idReg.addPassport(passportData).then(function success(){
-            res.send("Success!");
-        },function err(){
-            res.send("Error");
+router.post('/passport',[
+    check('passport-id').exists().isInt(),
+    check('name').exists().matches(/(a-zA-Z)*\s/),
+    check('DOB').exists(),
+    check('gender').exists(),
+    check('nationality').exists(),
+    check('idPhoto').exists(),
+    check('signPhoto').exists(),
+    check('countryName').exists(),
+    check('issueDate').exists(),
+    check('expiryDate').exists(),
+    check('placeOfBirth').exists().matches(/(a-zA-Z)*\s/),
+    check('placeOfIssue').exists().matches(/(a-zA-Z)*\s/),
+    check('password', 'passwords must be at least 5 chars long').isLength({ min: 5 }).matches(/(a-zA-Z1-9)*\s/), 
+    
+    check('confirmPassword', 'Password Confirmation field must have the same value as the password field').exists()
+    .custom((value, { req }) => value === req.body.password)
+], mulUpload,function(req,res){
+        var files = req.files;
+        var idPhotoStr = req.files['idPhoto'][0].buffer.toString('base64');
+        var signPhotoStr = req.files['signPhoto'][0].buffer.toString('base64');
+        var passportData = {
+            'formData':req.body,
+            'idPhotoStr': idPhotoStr,
+            'signPhotoStr': signPhotoStr
+        };
+        var idReg = new IdentityRegistry();
+        idReg.init().then(function(){
+            idReg.addPassport(passportData).then(function success(){
+                res.send("Success!");
+            },function err(){
+                res.send("Error");
+            });
         });
-    });
 });
 
 router.get('/passport/:id',function(req,res){
@@ -77,7 +100,7 @@ router.get('/passport/:id',function(req,res){
 });
 
 router.route('/government')
-    .post(function(req,res,next){
+    .post([check('name').exists().matches(/(a-zA-Z)*\s/)], function(req,res,next){
         var idReg = new IdentityRegistry();
         var key = new NodeRSA();
         key.generateKeyPair();
